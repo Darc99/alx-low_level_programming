7f3d67eb1ac8f5b5bbfd3d969b394160a35de3aa
Darc99 <lennyyhugoh@gmail.com>
2022-04-27 03:08:15 +0100

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,13 +8,20 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * 
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
