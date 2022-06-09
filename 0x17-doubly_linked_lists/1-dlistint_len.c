307a592abafd24c4d747c542a09a374f2a85767c
Darc99 <lennyyhugoh@gmail.com>
2022-06-10 00:20:20 +0100

    task0-1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,17 +4,15 @@
  *
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
 
 	while (h)
 	{
 		nodes++;
-		printf("%d\n", h->n);
 		h = h->next;
 	}
 
 	return (nodes);
 }
-
